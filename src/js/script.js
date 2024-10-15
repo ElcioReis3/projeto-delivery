d2e34a951945f17f7ad1149225520a13d59f9278
Élcio Reis <elcio.reis024@gmail.com>
2024-10-15 16:59:59 -0300

    Adicionado prev e next button

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,26 @@
-document.querySelectorAll('.content-button').forEach((element) => {
-    console.log(element)
-    element.addEventListener('click', (event) => {
-    })
-});
+let btn_prev = document.querySelector('#prev');
+let btn_next = document.querySelector('#next');
+let banner = document.querySelector('.banner');
+let arrayBanner = document.querySelectorAll('.banner');
+
+count = 0;
+let valor = 500;
+
+btn_prev.onclick = () => {
+    console.log(count)
+    if (count == 0) {
+        count = arrayBanner.length;
+    }
+    count--
+    banner.style.marginLeft = `${count * -500}px`
+
+}
+btn_next.onclick = () => {
+    console.log(count)
+    if (count == arrayBanner.length - 1) {
+        count = -1;
+    }
+    count++
+    banner.style.marginLeft = `${count * 500}px`
+
+}
